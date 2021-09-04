081fa41ff00987e086af15d0708ed67718694749
Nxrth-x <devilsouls666@gmail.com>
2021-09-04 19:53:54 -0300

    Fix: Updated setup metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
+
+ROOT = Path(__file__).parent
+
+LONG_DESCRIPTION = (ROOT / "README.md").read_text()
+
 
 setup(
     name="pyromises",
-    version="0.0.1",
+    version="0.0.2",
     description="Promises for Python",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     author="Eder Lima",
     author_email="lima.eder101@gmail.com",
+    keywords=["python", "async", "promises"],
     license="MIT",
     packages=["pyromises", "pyromises/promise"],
 )
